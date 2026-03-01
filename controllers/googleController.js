45aa83dd3502e0458c17614ea6d59cfc0c9249bf
samtopup23 <samtopup23@gmail.com>
2026-03-02 01:53:10 +0200

    googleController.js

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -46,7 +46,7 @@ const detailedSystemInstruction = `
 `.trim();
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     systemInstruction: detailedSystemInstruction,
 });
 
